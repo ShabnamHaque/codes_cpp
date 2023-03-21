5beadea28f52f1eb336a23d14aa12b479653a6a9
Shabnam <70348415+ShabnamHaque@users.noreply.github.com>
2023-03-21 22:26:46 +0530

    Update permutationsofstring02.cpp

--- permutationsofstring02.cpp
+++ permutationsofstring02.cpp
@@ -1,7 +1,8 @@
+
 #include<bits/stdc++.h>
 using namespace std;
-
-class Solution{
+class Solution
+{
 public:
   set<string> converttoset(vector<string> v)
   {
@@ -12,45 +13,43 @@ public:
     }
     return s;
   }
-
-  void permute(string s, int l, int r, vector<string> &ds)
+  void permute(string s,int index, vector<string> &ds)
   {
-
-    if (l == r)
+    if (index==s.size())
     {
       ds.push_back(s);
       return;
     }
-    for (int i = l; i <= r; i++)
+
+    for (int i = index; i <s.size(); i++)
     {
-      swap(s[l], s[i]);  // l starts from 0 
-      permute(s, l + 1, r, ds);
+      swap(s[index], s[i]);  // l starts from 0 
+      permute(s, index + 1, ds);
+      
    // after rching l==r we backtrack 
 
-      swap(s[l], s[i]);
+      swap(s[index], s[i]);
     }
   }
   vector<string> find_permutation(string s)
   {
     vector<string> ds;
-    permute(s, 0, s.size() - 1, ds);
+
+    permute(s, 0, ds);
+
     set<string> s1;
     s1 = converttoset(ds);
-
     vector<string> ds2(s1.begin(), s1.end());  //convert set to vector
-    
     return ds2;
   }
 };
 
 int main(){
-
     string a="ABCD";
     Solution X;
     vector<string>ans=X.find_permutation(a);
  for(int i=0;i<ans.size();i++){
     cout<<ans[i]<<"/";
     }
-  
     return 0;
 }
