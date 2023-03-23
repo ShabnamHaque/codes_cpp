32d1d12790c1f69fff55e30d60b9f3c7d47b1eca
Shabnam <70348415+ShabnamHaque@users.noreply.github.com>
2023-03-23 23:56:12 +0530

    Update permutationsofstring2.cpp

--- permutationsofstring2.cpp
+++ permutationsofstring2.cpp
@@ -60,3 +60,68 @@ int main()
     }
     return 0;
 }
+/*
+
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    set<string> converttoset(vector<string> v)
+    {
+        set<string> s;
+        for (auto it : v)
+        {
+            s.insert(it); // convert vector to set
+        }
+        return s;
+    }
+
+    void permute(string s,int index, vector<string> &ds)
+    {
+
+        if (index==s.size())
+        {
+            ds.push_back(s);
+
+            return;
+        }
+
+        for (int i = index; i < s.size(); i++)
+        {
+            swap(s[index], s[i]); // BFS
+            permute(s, index+1, ds);
+
+            swap(s[index], s[i]);
+        }
+    }
+    vector<string> find_permutation(string s)
+    {
+        vector<string> ds;
+
+        permute(s, 0,ds);
+
+        set<string> s1;
+        s1 = converttoset(ds);
+
+        vector<string> ds2(s1.begin(), s1.end()); // convert set to vector
+
+        return ds2;
+    }
+};
+
+int main()
+{
+
+    string a = "123";
+    Solution X;
+    vector<string> ans = X.find_permutation(a);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << "/";
+    }
+    return 0;
+}
+*/
